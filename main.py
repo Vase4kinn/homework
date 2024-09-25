134f1b9605ac3defe2cf07780184a3a2a5ca4b6c
Vasya <brisimichest@gmail.com>
2024-09-25 11:27:22 +0200

    сделал при помощи функции

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def f(a,b,c,d):
+    return ((-3+a**2)*b-2**3)/(c-2*d)
 a=int(input('a='))
 b=int(input('b='))
 c=int(input('c='))
 d=int(input('d='))
 
-print(((-3+a**2)*b-2**3)/(c-2*d))
+print(f(a,b,c,d))
